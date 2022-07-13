a80203bae8d9821538f3d954fe6bf2ea309232ab
dorcas-1995 <wayumulu58@gmail.com>
2022-07-13 10:33:44 -0400

    assignment commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <body>
+  
+    <div>please pass this test</div>
+  
+</body>;
 }
 
 export default Article;
